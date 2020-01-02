87bcf58d816984c31170a07cea2ea6154787f476
jycheng <783440482@qq.com>
2020-01-02 16:29:17 +0800

    富文本

--- src/views/communityManage/notice/add.vue
+++ src/views/communityManage/notice/add.vue
@@ -332,23 +332,24 @@ export default {
       // 配置表单
       'emoticon', // 表情
       'image', // 插入图片
-      // 'head', // 标题
-      // 'bold', // 粗体
+      'video',
+      'head', // 标题
+      'bold', // 粗体
       'fontSize', // 字号
-      // 'fontName', // 字体
-      // 'italic', // 斜体
-      // 'underline', // 下划线
-      // 'strikeThrough', // 删除线
+      'fontName', // 字体
+      'italic', // 斜体
+      'underline', // 下划线
+      'strikeThrough', // 删除线
       'foreColor', // 文字颜色
       'backColor', // 背景颜色
       'link', // 插入链接
-      // 'list', // 列表
+      'list', // 列表
       'justify', // 对齐方式
-      // 'quote', // 引用
-      // 'table', // 表格
-      // 'code', // 插入代码
+      'quote', // 引用
+      'table', // 表格
+      'code', // 插入代码
       'undo', // 撤销
-      // 'redo' // 重复
+      'redo' // 重复
     ]
     editor.create()
   }
